4e11a30de787a66fc0a21a8d1ac331261ff82717
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 11:52:39 +0300

    Added a dicstring to the Post class

--- models/models.py
+++ models/models.py
@@ -4,6 +4,7 @@ from db.database import Base
 
 
 class Post(Base):
+    """The template for the Post table in the database"""
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
