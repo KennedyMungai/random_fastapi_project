b80c72f2e0a1183c6c724350ac6942af9d3a1c66
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 10:35:57 +0300

    Added a docstring to the root api endpoint

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root() -> dict:
+    """The root api endpoint
+
+    Returns:
+        dict: Returns a message to show successful execution
+    """
     return {"message": "The api works"}
