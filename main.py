7b2d4b7ecde7ba050ecb93f665cd9b795719ea36
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 10:40:53 +0300

    Created a dummy create posts api

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ async def root() -> dict:
         dict: Returns a message to show successful execution
     """
     return {"message": "The api works"}
+
+
+@app.post("/createposts")
+async def create_posts() -> dict:
+    """A dummy post creator endpoint
+
+    Returns:
+        dict: A message to show successful code execution
+    """
+    return {"message": "The post has been successfully created"}
