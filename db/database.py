87ff198da4a5f8cd9d2a87a631b4de025d0836fd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 12:08:58 +0300

    Rearranged the imports in the file

--- db/database.py
+++ db/database.py
@@ -1,11 +1,10 @@
 """The database connection file"""
 import os
 
-from dotenv import load_dotenv, find_dotenv
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 
 load_dotenv(find_dotenv())
 
