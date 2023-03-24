70d8ae3bb6dbe3975f5fd34d2fdb749e9b99e831
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 11:36:37 +0300

    Implemented the database path from the system environment

--- db/database.py
+++ db/database.py
@@ -1,9 +1,10 @@
+import os
+from os.path import dirname, join
+
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from os.path import dirname, join
-from dotenv import find_dotenv, load_dotenv
 
 
 dotenv_path = join(dirname(__file__), '.env')
