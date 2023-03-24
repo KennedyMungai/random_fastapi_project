e445480d224a9896c29d31f309b29e38ee39ebfc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 13:09:06 +0300

    Created the Schema file for the Posts

--- schemas/PostSchemas.py
+++ schemas/PostSchemas.py
@@ -1,2 +1,13 @@
 """Ceeated the file containing the Posts Schemas"""
 from pydantic import BaseModel
+
+
+class CreatePost(BaseModel):
+    """The template for teh Create Post data
+
+    Args:
+        BaseModel (Class): The parent class for the CreatePost class
+    """
+    title: str
+    content: str
+    published: bool
