010aa92686a61b10c77f41569ba5f76d3b65d00f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 15:40:14 +0300

    Refactored tyhej schemas script

--- schemas/PostSchemas.py
+++ schemas/PostSchemas.py
@@ -2,12 +2,21 @@
 from pydantic import BaseModel
 
 
-class CreatePost(BaseModel):
+class PostBase(BaseModel):
+    """The base class representing the Post daya
+
+    Args:
+        BaseModel (Class): The parent class
+    """
+    title: str
+    content: str
+    published: bool
+
+
+class PostCreate(PostBase):
     """The template for teh Create Post data
 
     Args:
         BaseModel (Class): The parent class for the CreatePost class
     """
-    title: str
-    content: str
-    published: bool
+    pass
