58f6e7ae842639cb8020c1c45ac1601ebf1931f3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 11:40:43 +0300

    Deleted the import for find_dotenv

--- db/database.py
+++ db/database.py
@@ -1,7 +1,7 @@
 import os
 from os.path import dirname, join
 
-from dotenv import find_dotenv, load_dotenv
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
