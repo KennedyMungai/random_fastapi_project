39a8232b3a9652eb04e04dbc4c80708ae0c879ff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 16:11:36 +0300

    Created the Users model

--- models/models.py
+++ models/models.py
@@ -14,3 +14,15 @@ class Post(Base):
     content = Column(String(255), nullable=False)
     published = Column(Boolean, default=1, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), default=datetime.utcnow())
+
+
+class User(Base):
+    """The model for the db user tab;e
+
+    Args:
+        Base (Class): A declarative base for the database and such
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(255), nullable=False, unique=True)
+    password = Column(String, nullable=False)
