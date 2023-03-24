42c21c412224f7ecd7c12e48a9752a4df44cc279
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 12:14:45 +0300

    Created a dummy endpoint that interacts with the database

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 """The main file for the project"""
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 
 from db.database import engine, get_db
 from models import models
+from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -17,3 +18,8 @@ async def root() -> dict:
         dict: Returns a message to show successful execution
     """
     return {"message": "The api works"}
+
+
+@app.get("/sqlalchemy")
+async def test_posts(_db: Session = Depends(get_db)):
+    pass
