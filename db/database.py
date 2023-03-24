352f672011fb9ae5a202d8704e7ad2ac99840485
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 11:33:42 +0300

    LOaded the environment variable file

--- db/database.py
+++ db/database.py
@@ -1,6 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from os.path import dirname, join
+from dotenv import find_dotenv, load_dotenv
 
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@localhost/{DATABASE_NAME}'
