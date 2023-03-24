4e12f4c864bffde92b00de4192f255da46732259
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-24 12:22:51 +0300

    Added integers at the end of the Strings

--- models/models.py
+++ models/models.py
@@ -7,6 +7,6 @@ class Post(Base):
     """The template for the Post table in the database"""
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False)
-    title = Column(String, nullable=False)
-    content = Column(String, nullable=False)
+    title = Column(String(255), nullable=False)
+    content = Column(String(255), nullable=False)
     published = Column(Boolean, default=True)
