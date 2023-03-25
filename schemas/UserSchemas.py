beeb4db2d770fe5ccbcbed80b9315e1b6a7153e8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-25 08:56:25 +0300

    Refactored the entire script

--- schemas/UserSchemas.py
+++ schemas/UserSchemas.py
@@ -11,6 +11,24 @@ class UserBase(BaseModel):
     email: EmailStr
     password: str
 
+
+class UserRequest(UserBase):
+    """The user request schema
+
+    Args:
+        UserBase (Class): The base user type
+    """
+    pass
+
+
+class UserResponse(BaseModel):
+    """The response model for the user
+
+    Args:
+        BaseModel (Class): The parent class
+    """
+    email: EmailStr
+
     class Config:
-        """The config class for the UserBase class"""
+        """The config class for the UserResponse class"""
         orm_mode = True
