6e5f6b8f10739660b761d68f99e02555ead431f1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-25 09:21:54 +0300

    Moved all the hashing logic to this file

--- utils.py
+++ utils.py
@@ -1,1 +1,8 @@
 """A file that has a bunch of random utils"""
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash(password: str):
+    return pwd_context.hash(password)
