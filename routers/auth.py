bc17fad1f2db92e8bfbd078fe058220cf9f2aa50
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-25 11:54:29 +0300

    Created the Login router

--- routers/auth.py
+++ routers/auth.py
@@ -1,2 +1,5 @@
 """Stores all the authentication logic for the API"""
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+
+
+login_router = APIRouter(prefix="/login", tags=["Login"])
