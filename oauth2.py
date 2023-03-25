7abdbc8b3703727579c4962ad7514a239f5de729
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-25 12:33:33 +0300

    Properly loaded the dortenv packages

--- oauth2.py
+++ oauth2.py
@@ -4,6 +4,6 @@ import os
 from dotenv import find_dotenv, load_dotenv
 from jose import JWTError, jwt
 
-_env = load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
