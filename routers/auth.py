f89e1e129cb26cbfa49ce98145345a561cdcaed4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-25 12:06:11 +0300

    Wrote some logic in the login function

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,22 @@
 """Stores all the authentication logic for the API"""
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from sqlalchemy.orm.session import Session
+
+from db.database import get_db
+from models.models import User
+from schemas.UserLogin import UserLogin
 
 
 login_router = APIRouter(prefix="/login", tags=["Authentication"])
+
+
+@login_router.post("/")
+async def login(_user_credentials: UserLogin, _db: Session = Depends(get_db)):
+    _user = _db.query(User).filter(
+        User.email == _user_credentials.email).first()
+
+    if not _user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid credentials"
+        )
