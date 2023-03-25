39eb3350b78a0d1831a394fb2a24bc51b808f744
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-25 09:23:17 +0300

    Added a docstring to the password hash function

--- utils.py
+++ utils.py
@@ -4,5 +4,13 @@ from passlib.context import CryptContext
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def password_hash(password: str):
+def password_hash(password: str) -> str:
+    """A function that spits out hashed passwords
+
+    Args:
+        password (str): The password
+
+    Returns:
+        Str: A hash version of the password
+    """
     return pwd_context.hash(password)
