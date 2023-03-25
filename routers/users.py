4d411a32be99024a08d446d9d1672f24a0bb464b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-25 09:52:36 +0300

    Moved all the user router logic to its own file

---
+++ routers/users.py
@@ -0,0 +1,65 @@
+"""The Users routing information"""
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm.session import Session
+
+from db.database import get_db
+from models.models import User
+from schemas.UserSchemas import UserRequest, UserResponse
+from utils import password_hash
+
+users_router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@users_router.post(
+    "/users",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserResponse
+)
+async def create_user(_new_user: UserRequest, _db: Session = Depends(get_db)):
+    """An endpoint to create Users
+
+    Args:
+        _new_user (UserBase): The new user info
+        _db (Session, optional): The database connection. Defaults to Depends(get_db).
+
+    Returns:
+        UserBase: The newly created user
+    """
+    # Hashing the password
+    _hashed_password = password_hash(_new_user.password)
+    _new_user.password = _hashed_password
+
+    _user = User(**_new_user.dict())
+    _db.add(_user)
+    _db.commit()
+    _db.refresh(_user)
+
+    return _user
+
+
+@users_router.get(
+    "/users/{_id}",
+    response_model=UserResponse
+)
+async def retrieve_one_user(_id: int, _db: Session = Depends(get_db)) -> UserResponse:
+    """An endpoint to retrieve one user
+
+    Args:
+        _id (int): The id of the user
+        _db (Session, optional): The database session. Defaults to Depends(get_db).
+
+    Raises:
+        HTTPException: A 404 is raised if the user is not found in the database
+
+    Returns:
+        UserResponse: User data formatted in the User Response schema is given back
+    """
+    _user = _db.query(User).filter(User.id == _id).first()
+
+    if not _user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The user with id {_id} not found"
+        )
+
+    return _user
