0b9495c2261a096f243497858c05052b040e30be
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-27 11:16:51 +0300

    Wrote a finction to verify the access token

--- oauth2.py
+++ oauth2.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 from dotenv import find_dotenv, load_dotenv
 from jose import jwt
 
+from schemas.user_schemas import TokenData
+
 load_dotenv(find_dotenv())
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -29,3 +31,22 @@ def create_access_token(_data: dict):
     _encoded_jwt = jwt.encode(_to_encode, SECRET_KEY, ALGORITHM)
 
     return _encoded_jwt
+
+
+def verify_access_token(_token: str, _credentials_exception):
+    """A function to verify the access token
+
+    Args:
+        _token (str): The access token
+        _credentials_exception (Exception): The passed in exception
+
+    Raises:
+        _credentials_exception: The exception passed in
+    """
+    _payload = jwt.decode(_token, SECRET_KEY, ALGORITHM)
+    _id: str = _payload.get("user_id")
+
+    if not _id:
+        raise _credentials_exception
+
+    _token_data = TokenData(id=_id)
