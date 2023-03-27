40c63d1d3bdfb7ddd7d365d567f0238662e37098
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-27 12:11:31 +0300

    Sorted the imports

--- oauth2.py
+++ oauth2.py
@@ -3,12 +3,11 @@ import os
 from datetime import datetime, timedelta
 
 from dotenv import find_dotenv, load_dotenv
-from fastapi import Depends, status, HTTPException
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 
 from schemas.user_schemas import TokenData
-
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
