8c62d8bfe1d1d3d8b69464834a3d3fa3932614a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-28 15:44:18 +0300

    Created the vote endpoint

--- routers/vote.py
+++ routers/vote.py
@@ -1,5 +1,18 @@
 """The router file for the vote"""
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
+from schemas.vote_schemas import Vote
+from db.database import get_db
+from sqlalchemy.orm.session import Session
+from oauth2 import get_current_user
 
 
 vote_router = APIRouter(prefix='/vote', tags=['votes'])
+
+
+@vote_router.post("/", status_code=status.HTTP_201_CREATED)
+async def vote(
+    _vote: Vote,
+    _db: Session = Depends(get_db),
+    _current_user=Depends(get_current_user)
+):
+    pass
