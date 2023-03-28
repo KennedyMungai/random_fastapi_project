d63dda5c07d49d657d15aa23d9764a88d43fb332
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-28 15:39:34 +0300

    Added a docstring to the Vote class

--- schemas/vote_schemas.py
+++ schemas/vote_schemas.py
@@ -5,5 +5,10 @@ from pydantic import BaseModel, conint
 
 
 class Vote(BaseModel):
+    """The vote schema class
+
+    Args:
+        BaseModel (Class): The parent class
+    """
     post_id: int
     direction: Optional[conint(le=1)]
