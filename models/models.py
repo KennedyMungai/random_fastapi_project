b05fed8c4d69684ed0a422b60dc2c386161b49b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-28 14:21:21 +0300

    Updated the names of the database tables for the Votes

--- models/models.py
+++ models/models.py
@@ -40,7 +40,7 @@ class Votes(Base):
         Base (A function): Does something
     """
     __tablename__ = "votes"
-    user_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"),
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"),
                      primary_key=True, nullable=False)
-    post_id = Column(Integer, ForeignKey("post.id", ondelete="CASCADE"),
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"),
                      primary_key=True, nullable=False)
