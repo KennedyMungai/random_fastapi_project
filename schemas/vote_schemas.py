912ae8391dcfea4046ed5a1195be7db8d5f34647
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-28 15:39:04 +0300

    Created teh vote schema Class

--- schemas/vote_schemas.py
+++ schemas/vote_schemas.py
@@ -1,1 +1,9 @@
 """The file that contains the votes schema"""
+from typing import Optional
+
+from pydantic import BaseModel, conint
+
+
+class Vote(BaseModel):
+    post_id: int
+    direction: Optional[conint(le=1)]
